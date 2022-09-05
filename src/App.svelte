399fc0f488bf849679a4f731c9fe3c06a3fdc213
Hariss Ali Gills <harissgills@gmail.com>
2022-09-06 01:18:34 +0300

    Added navbar

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,11 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import svelteLogo from "./assets/svelte.svg";
+  import Counter from "./lib/Counter.svelte";
+  import Navbar from "./lib/Navbar.svelte";
+  import Drawer from "./lib/Drawer.svelte";
 </script>
 
+<Drawer/>
 <main>
   <div>
     <a href="https://vitejs.dev" target="_blank">
@@ -19,12 +22,12 @@
   </div>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank"
+      >SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
   </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style>
