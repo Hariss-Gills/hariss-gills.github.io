45e0385ff33ebe76911ac1b74e0279ca44b5b7d1
Hariss Ali Gills <harissgills@gmail.com>
2022-09-06 16:03:33 +0300

    Added drawer and Navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -2,26 +2,23 @@
   export let drawerId;
 </script>
 
-<div class="fixed inset-x-0 top-0 navbar bg-base-100">
-  <div class="navbar-start">
-    <button tabindex="0" class="btn btn-ghost btn-circle">
-      <label for={drawerId}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          ><path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h7"
-          /></svg
-        >
-      </label>
-      <button />
-    </button>
+<div class="fixed inset-x-0 top-0 w-full navbar bg-base-300">
+  <div class="navbar-start flex-none">
+    <label for={drawerId} class="btn btn-square btn-ghost">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        ><path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M4 6h16M4 12h16M4 18h7"
+        /></svg
+      >
+    </label>
   </div>
   <div class="navbar-center">
     <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
