5df4cc8dd7eb5ed8dbc0dd4531468d734f9238a2
Hariss Ali Gills <harissgills@gmail.com>
2022-09-06 16:03:00 +0300

    Added Drawer and Navbar

--- src/lib/Drawer.svelte
+++ src/lib/Drawer.svelte
@@ -2,17 +2,18 @@
   import Navbar from "./Navbar.svelte";
 </script>
 
-<div class="drawer">
+<div class="drawer absolute inset-0">
   <input id="my-drawer" type="checkbox" class="drawer-toggle" />
   <div class="drawer-content">
     <!-- Page content here -->
     <Navbar drawerId={"my-drawer"} />
   </div>
+
   <div class="drawer-side">
-    <label for="my-drawer" class="drawer-overlay" />
-    <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+    <label for="my-drawer" class="drawer-overlay"/>
+    <ul class="menu p-4 overflow-y-auto w-80 bg-base-300Y text-base-content">
       <!-- Sidebar content here -->
-      <li><a>Sidebar Item 1</a></li>
+       <li><a>Sidebar Item 1</a></li>
       <li><a>Sidebar Item 2</a></li>
     </ul>
   </div>
